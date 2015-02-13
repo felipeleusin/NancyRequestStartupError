0d88bb323f61071462d2ee3b5dbb1d664d095036
Felipe Amorim <felipe.leusin@gmail.com>
2015-02-13 15:20:10 -0200

    error repro

---
+++ NancyRequestStartupError.Tests/ConfigurableBootstrapperTest.cs
@@ -0,0 +1,25 @@
+﻿namespace NancyRequestStartupError.Tests
+{
+    using Nancy;
+    using Nancy.Testing;
+    using Xunit;
+
+    public class ConfigurableBootstrapperTest
+    {
+        [Fact]
+        public void should_not_automatically_execute_TestRequestStartup()
+        {
+            // Given
+            var browser = new Browser(with =>
+            {
+                with.Module<TestModule>();
+            });
+
+            // When
+            var response = browser.Get("/");
+
+            // Then
+            Assert.Equal(response.StatusCode, HttpStatusCode.OK);
+        }
+    }
+}
---
+++ NancyRequestStartupError/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace NancyRequestStartupError
+{
+    using Nancy;
+    using Nancy.Bootstrapper;
+
+    public class TestRequestStartup : IRequestStartup
+    {
+        public void Initialize(IPipelines pipelines, NancyContext context)
+        {
+            pipelines.BeforeRequest.AddItemToStartOfPipeline(nancyContext =>
+            {
+                return HttpStatusCode.UnprocessableEntity;
+            });
+        }
+    }
+
+    public class TestModule : NancyModule
+    {
+        public TestModule()
+        {
+            Get["/"] = o =>
+            {
+                return HttpStatusCode.OK;
+            };
+        }
+    }
+}
